51b63e9f305424e18b04e856e88a3b5933d08376
lumineade <imlumineade@gmail.com>
2026-05-18 18:07:08 -0700

    Initial commit

---
+++ src/main/java/net/luminyra/lweapon/LumisWeaponsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.luminyra.lweapon;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LumisWeaponsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
