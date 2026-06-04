5c1ca14eb6cc0db26e452fe4b76fad587c0ed44d
lumineade <imlumineade@gmail.com>
2026-06-03 22:42:45 -0700

    HELLPP MEEEE I WANT TO DIIIEEEE

--- src/main/java/net/luminyra/lweapon/datagen/ModModelProvider.java
+++ src/main/java/net/luminyra/lweapon/datagen/ModModelProvider.java
@@ -5,7 +5,15 @@ import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.luminyra.lweapon.Item.ModItems;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.ItemModelUtils;
 import net.minecraft.client.data.models.model.ModelTemplates;
+import net.minecraft.client.renderer.item.properties.select.DisplayContext;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
+
+import java.util.Arrays;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricPackOutput output) {
@@ -21,10 +29,26 @@ public class ModModelProvider extends FabricModelProvider {
     public void generateItemModels(ItemModelGenerators itemModelGenerators) {
         itemModelGenerators.generateFlatItem(ModItems.NIPZIUM_INGOT, ModelTemplates.FLAT_ITEM);
 
-        itemModelGenerators.generateFlatItem(ModItems.HEAVY_LIGHT, ModelTemplates.FLAT_ITEM);
+        createSimpleGuiVarying(itemModelGenerators, ModItems.HEAVY_LIGHT);
 
         //itemModelGenerators.declareCustomModelItem(ModItems.HEAVY_LIGHT);
 
         //bro im actuall y gonig to kill a small child im goin im to im actually going to do it this time
+    }
+
+    public static void createSimpleGuiVarying(ItemModelGenerators generator, Item item) {
+        Identifier id = BuiltInRegistries.ITEM.getKey(item);
+
+        generator.createFlatItemModel(item, ModelTemplates.FLAT_ITEM);
+        generator.itemModelOutput.accept(item,
+                ItemModelUtils.select(
+                        new DisplayContext(),
+                        ItemModelUtils.plainModel(id.withPath(st -> "item/" + st + "_in_hand")),
+                        ItemModelUtils.when(
+                                Arrays.asList(ItemDisplayContext.GUI, ItemDisplayContext.GROUND, ItemDisplayContext.FIXED),
+                                ItemModelUtils.plainModel(id.withPath(st -> "item/" + st))
+                        )
+                )
+        );
     }
 }
